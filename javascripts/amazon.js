50767de8269a7ddb61922a73e1e419194daeac64
lghuy05 <luonggiahuy1909@gmail.com>
2025-09-23 21:55:54 -0400

    feat: cart quantity feature

--- javascripts/amazon.js
+++ javascripts/amazon.js
@@ -67,7 +67,13 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
       cart.push({ productId: productId, quantity: 1 });
 
     }
-    console.log(cart);
+
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
+    });
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+    console.log(cartQuantity);
   });
 
 });
