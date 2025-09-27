7cea4ddb4dd9d0778879eba495aa9d4044f5bc1a
lghuy05 <luonggiahuy1909@gmail.com>
2025-09-27 17:26:10 -0400

    feat: delete cart

--- javascripts/checkout.js
+++ javascripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from '../data/cart.js';
+import { cart, removefromCart } from '../data/cart.js';
 import { products } from '../data/products.js';
 let matchingproducts;
 let cartSummaryHTML = '';
@@ -32,7 +32,7 @@ cart.forEach((cartItem) => {
         <span class="update-quantity-link link-primary">
           Update
         </span>
-        <span class="delete-quantity-link link-primary">
+        <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingproducts.id}">
           Delete
         </span>
       </div>
@@ -82,3 +82,9 @@ cart.forEach((cartItem) => {
  `;
 });
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+  link.addEventListener('click', () => {
+    let productId = link.dataset.productId;
+    removefromCart(productId);
+  })
+})
